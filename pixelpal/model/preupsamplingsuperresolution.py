5acb98a5044be2e14e2ea8e70458c37fe79d66dd
Michal PARUSINSKI <michal@parusinski.me>
2020-04-12 18:43:02 +0200

    feat: implementation of two standard and simple super resolution models
    1. Implementation of the pre upsampling super resolution model
    2. Implementation of the post upsampling super resolution model
    
    However it still has not been been throughly tested

---
+++ pixelpal/model/preupsamplingsuperresolution.py
@@ -0,0 +1,27 @@
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Input, UpSampling2D, Conv2D
+
+from pixelpal.model.base import AbstractAugmentor
+
+
+class PreUpsamplingSuperResolution(AbstractAugmentor):
+
+    def __init__(self):
+        super().__init__()
+
+    def build_model(self, input_shape=(32, 32), channels=4, **kwargs):
+        filters = kwargs.get('filters', 64)
+        kernel_size = kwargs.get('kernel_size', 3)
+        num_convolution_layers = kwargs.get('num_convolution_layers', 2)
+
+        input_layer = Input(shape=(*input_shape, channels))
+        # TODO: Should check if it possible to do cubic interpolation
+        upscale = UpSampling2D(size=(2, 2), interpolation='bilinear')(input_layer)
+
+        previous_layer = upscale
+        for i in range(num_convolution_layers):
+            previous_layer = Conv2D(filters, kernel_size, padding="same")(previous_layer)
+
+        reconstruction = Conv2D(channels, 1, padding="same")(previous_layer)
+        self.model = Model(inputs=input_layer, outputs=reconstruction)
+
